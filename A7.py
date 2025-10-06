89a515ae8913d4462958ebb218105ab3b98aa450
Мария Бондаренко <Bondarenko_MA_25@student.grsu.by>
2025-10-06 16:51:43 +0300

    Add files via upload

---
+++ A7.py
@@ -0,0 +1,16 @@
+x1=int(input())
+y1=int(input())
+x2=int(input())
+y2=int(input())
+def get_color(x,y):
+    return (x+y)%2==0
+def get_color_name(white):
+    return 'White' if white else 'Black'
+color1 = get_color(x1,y1)
+color2 = get_color(x2,y2)
+if color1==color2:
+    print("YES")
+    color_name = get_color_name(color1)
+    print(color_name)
+else:
+    print("NO")
