f0a4c23531bf8bbfa692fb3699acf761f49f425c
Бондаренко Мария <bondarenko_ma_25@student.grsu.by>
2025-10-20 21:03:20 +0300

    Точки координат не должны быть равны 0

--- Lab_3/A1_3.py
+++ Lab_3/A1_3.py
@@ -2,7 +2,7 @@ x1=int(input())
 y1=int(input())
 x2=int(input())
 y2=int(input())
-if x1==0 or y1==0 or x2==0 or y2==0:
+if x1==0 or y1==0 or x2==0 or y2==0: 
 	print ("Ошибка")
 else:
 	if (x1>0 and y1>0): 
