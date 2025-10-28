567ef5367a0bf8948271aee56d8b25c99fae345e
Бондаренко Мария <bondarenko_ma_25@student.grsu.by>
2025-10-28 09:14:42 +0300

    Вывод статистики

--- Lab_4/A1_4.py
+++ Lab_4/A1_4.py
@@ -11,7 +11,7 @@ for i in range(1, n + 1):
 
     a = random.randint(2, 9)
     b = random.randint(2, 9)
-    correct_answer = a * b 
+    correct_answer = a * b
 
     while True:
         start_time = time.time()
@@ -37,4 +37,4 @@ print("===========================================================")
 print(f"Общее время: {total_time:.1f} секунд")
 print(f"Среднее время на вопрос: {total_time/n:.1f} сек")
 print(f"Правильных ответов: {correct_count}/{n}")
-print(f"Процент правильных: {correct_count/n*100:.1f}%")
+print(f"Процент правильных: {correct_count/n*100:.1f}%") 
