9fd4aaac040faf340cef49efcb794b0001405b39
Мария Бондаренко <Bondarenko_MA_25@student.grsu.by>
2025-11-18 07:30:48 +0300

    счетчик кол-ва предложений

--- Lab_5/A2.py
+++ Lab_5/A2.py
@@ -2,7 +2,7 @@ import re
 
 text = 'He jests at scars. That never felt a wound! Hello, friend! Are you OK?' 
 sentences = re.split(r'(?<=[.?!]) ', text) 
-count = 0
+count = 0 
 for sentence in sentences:
     print(sentence, end='\n')
 for sentence in sentences:
@@ -10,3 +10,4 @@ for sentence in sentences:
         count += 1
 
 print(f'Предложений в тексте: {count}')
+
