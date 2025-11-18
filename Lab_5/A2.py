5022f2e5ffce9d5cb4bc4ca6923752c4dac20e8b
Мария Бондаренко <Bondarenko_MA_25@student.grsu.by>
2025-11-18 07:32:04 +0300

    вывод кол-ва предложений

--- Lab_5/A2.py
+++ Lab_5/A2.py
@@ -5,10 +5,10 @@ sentences = re.split(r'(?<=[.?!]) ', text)
 count = 0 
 for sentence in sentences: 
     print(sentence, end='\n') 
-for sentence in sentences:
-    if sentence in text:
-        count += 1
+for sentence in sentences: 
+    if sentence in text: 
+        count += 1 
+print(f'Предложений в тексте: {count}') 
 
-print(f'Предложений в тексте: {count}')
 
 
