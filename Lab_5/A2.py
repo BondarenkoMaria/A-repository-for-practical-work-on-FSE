18e9cdaf11d0b6627c536224915e143ded18399a
Мария Бондаренко <Bondarenko_MA_25@student.grsu.by>
2025-11-18 07:31:18 +0300

    вывод предложений

--- Lab_5/A2.py
+++ Lab_5/A2.py
@@ -3,11 +3,12 @@ import re
 text = 'He jests at scars. That never felt a wound! Hello, friend! Are you OK?' 
 sentences = re.split(r'(?<=[.?!]) ', text) 
 count = 0 
-for sentence in sentences:
-    print(sentence, end='\n')
+for sentence in sentences: 
+    print(sentence, end='\n') 
 for sentence in sentences:
     if sentence in text:
         count += 1
 
 print(f'Предложений в тексте: {count}')
+
 
