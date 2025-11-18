6fa1dca0ae238d1328640c320239593a53aa817b
Мария Бондаренко <Bondarenko_MA_25@student.grsu.by>
2025-11-18 07:33:35 +0300

    делим текст на буквы

--- Lab_5/A3.py
+++ Lab_5/A3.py
@@ -1,6 +1,7 @@
-text = input('Ведите текст: ')
-words = text.split()
+text = input('Ведите текст: ') 
+words = text.split() 
 for word in words:
     if len(word) >= 3:
         first_letter = word[0].upper()
+
         print(first_letter, end='')
