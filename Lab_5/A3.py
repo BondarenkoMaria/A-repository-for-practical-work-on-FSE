8ce7c847e8ca9b799e1ce6a86f8dbbf60ab83552
Мария Бондаренко <Bondarenko_MA_25@student.grsu.by>
2025-11-18 07:34:20 +0300

    поиск и вывод необходимых букв

--- Lab_5/A3.py
+++ Lab_5/A3.py
@@ -1,7 +1,7 @@
 text = input('Ведите текст: ') 
 words = text.split() 
-for word in words:
-    if len(word) >= 3:
-        first_letter = word[0].upper()
+for word in words: 
+    if len(word) >= 3: 
+        first_letter = word[0].upper() 
+        print(first_letter, end='') 
 
-        print(first_letter, end='')
